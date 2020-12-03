795d1b2f9fbd216a4dd3586cb865b61737dd6d09
Kirsty Mullen <kirstyvox@gmail.com>
2020-12-03 20:29:15 +0000

    feat: add sign in component

--- src/pages/sign-in-or-register/sign-in-or-register.component.jsx
+++ src/pages/sign-in-or-register/sign-in-or-register.component.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import './sign-in-or-register.component.styles.scss';
+import SignIn from '../../components/sign-in/sign-in.component';
 
 const SignInOrRegister = () => (
-  <div className='sign-in-or-register'>SIGN IN</div>
+  <div className='sign-in-or-register'>
+    <SignIn />
+  </div>
 );
 
 export default SignInOrRegister;
