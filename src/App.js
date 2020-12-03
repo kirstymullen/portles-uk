ec84fe44cf916ec944d372d6d06e951aeead71fa
Kirsty Mullen <kirstyvox@gmail.com>
2020-12-03 19:04:27 +0000

    feat: shop page creation

--- src/App.js
+++ src/App.js
@@ -2,13 +2,8 @@ import React from 'react';
 import {Route, Switch} from 'react-router-dom';
 import './App.css';
 
-import HomePage from './pages/homepage.component';
-
-const HatsPage = () => (
-  <div>
-    <h1>Hats Page</h1>
-  </div>
-);
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
@@ -17,8 +12,8 @@ function App() {
         <Route exact path='/'>
           <HomePage />
         </Route>
-        <Route path='/shop/hats'>
-          <HatsPage />
+        <Route path='/shop'>
+          <ShopPage />
         </Route>
       </Switch>
     </div>
