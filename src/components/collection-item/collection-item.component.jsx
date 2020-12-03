32c92a541646cdbebc706a2388c8ec85c8164be2
Kirsty Mullen <kirstyvox@gmail.com>
2020-12-03 19:37:44 +0000

    feat: ability to preview collections on the shop page

---
+++ src/components/collection-item/collection-item.component.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './collection-item.styles.scss';
+
+const CollectionItem = ({id, name, price, imageUrl}) => (
+  <div className='collection-item'>
+    <div className='image' style={{backgroundImage: `url(${imageUrl})`}} />
+    <div className='collection-footer'>
+      <span className='name'>{name}</span>
+      <span className='price'>{price}</span>
+    </div>
+  </div>
+);
+
+export default CollectionItem;
+
+CollectionItem.propTypes = {
+  id: PropTypes.number,
+  name: PropTypes.string,
+  price: PropTypes.number,
+  imageUrl: PropTypes.string,
+};
