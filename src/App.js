d69f6ab780dd669956d11e6bf3e1a8a3c5331f54
Kirsty Mullen <kirstyvox@gmail.com>
2020-12-03 20:03:57 +0000

    feat: add header component

--- src/App.js
+++ src/App.js
@@ -4,18 +4,22 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 function App() {
   return (
     <div>
-      <Switch>
-        <Route exact path='/'>
-          <HomePage />
-        </Route>
-        <Route path='/shop'>
-          <ShopPage />
-        </Route>
-      </Switch>
+      <Header />
+      <div className='page-container'>
+        <Switch>
+          <Route exact path='/'>
+            <HomePage />
+          </Route>
+          <Route path='/shop'>
+            <ShopPage />
+          </Route>
+        </Switch>
+      </div>
     </div>
   );
 }
