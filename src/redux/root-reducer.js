dabee135d363e9b498e1fa069a944c3b5b7b8e37
Kirsty Mullen <kirstyvox@gmail.com>
2020-12-22 11:50:18 +0000

    feat: basket persisted in local storage

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,19 @@
 import {combineReducers} from 'redux';
+import {persistReducer} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import userReducer from './user/user.reducer';
 import basketReducer from './basket/basket.reducer';
 
-export default combineReducers({
+const persistConfiguration = {
+  key: 'root',
+  storage,
+  whitelist: ['basket'],
+};
+
+const rootReducer = combineReducers({
   user: userReducer,
   basket: basketReducer,
 });
+
+export default persistReducer(persistConfiguration, rootReducer);
