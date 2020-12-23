f15a3647c9ed3342339524497f6a00fe06baa663
Kirsty Mullen <kirstyvox@gmail.com>
2020-12-23 10:30:02 +0000

    feat: collection component displays items

--- src/pages/collection/collection.components.jsx
+++ src/pages/collection/collection.components.jsx
@@ -4,13 +4,24 @@ import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 import {selectShopCollection} from '../../redux/shop/shop.selectors';
 
-//mport CollectionItem from '../../components/collection-item/collection-item.component';
+import CollectionItem from '../../components/collection-item/collection-item.component';
 
-const CollectionPage = ({collection}) => (
-  <div className='collection-page'>
-    <h2>{collection ? collection.title : 'COLLECTION NOT FOUND'}</h2>
-  </div>
-);
+const CollectionPage = ({collection}) => {
+  const {title, items} = collection || {
+    title: 'COLLECTION NOT FOUND',
+    items: [],
+  };
+  return (
+    <div className='collection-page'>
+      <h2 className='title'>{title}</h2>
+      <div className='items'>
+        {items.map(item => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 const mapStateToProps = (state, ownProps) => ({
   collection: selectShopCollection(ownProps.match.params.collectionId)(state),
