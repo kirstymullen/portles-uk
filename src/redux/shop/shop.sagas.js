77768ae0289db94525f54d2820d12a1506d2de4d
Kirsty Mullen <kirstyvox@gmail.com>
2021-01-06 20:02:18 +0000

    internal: shop sagas wip

---
+++ src/redux/shop/shop.sagas.js
@@ -0,0 +1,13 @@
+import {takeEvery} from 'redux-saga/effects';
+import ShopActionTypes from './shop.types';
+
+export function* fetchCollectionsAsync() {
+  yield console.log('fetch collections async fired');
+}
+
+export function* fetchCollectionsStart() {
+  yield takeEvery(
+    ShopActionTypes.FETCH_COLLECTIONS_SUCCESS,
+    fetchCollectionsAsync
+  );
+}
