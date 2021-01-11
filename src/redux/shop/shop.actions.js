3c5ce672b2d2b1539cab1148df11011d6f172eba
Kirsty Mullen <kirstyvox@gmail.com>
2021-01-11 19:18:31 +0000

    feat: collections fetched using a saga

--- src/redux/shop/shop.actions.js
+++ src/redux/shop/shop.actions.js
@@ -1,8 +1,4 @@
 import ShopActionTypes from './shop.types';
-import {
-  firestore,
-  convertCollectionsSnapshotToMap,
-} from '../../firebase/firebase.utils';
 
 export const fetchCollectionsStart = () => ({
   type: ShopActionTypes.FETCH_COLLECTIONS_START,
@@ -17,19 +13,3 @@ export const fetchCollectionsFailure = errorMessage => ({
   type: ShopActionTypes.FETCH_COLLECTIONS_FAILURE,
   payload: errorMessage,
 });
-
-export const fetchCollectionsStartAsync = () => {
-  return dispatch => {
-    const collectionsRef = firestore.collection('collections');
-    dispatch(fetchCollectionsStart());
-
-    collectionsRef
-      .get()
-      .then(snapShot => {
-        const collectionsMap = convertCollectionsSnapshotToMap(snapShot);
-        dispatch(fetchCollectionsSuccess(collectionsMap));
-        this.setState({loadingCollections: false});
-      })
-      .catch(error => dispatch(fetchCollectionsFailure(error.message)));
-  };
-};
--- src/redux/shop/shop.sagas.js
+++ src/redux/shop/shop.sagas.js
@@ -1,8 +1,26 @@
-import {takeEvery} from 'redux-saga/effects';
+import {takeEvery, call, put} from 'redux-saga/effects';
+import {
+  firestore,
+  convertCollectionsSnapshotToMap,
+} from '../../firebase/firebase.utils';
+
 import ShopActionTypes from './shop.types';
+import {fetchCollectionsSuccess, fetchCollectionsFailure} from './shop.actions';
 
 export function* fetchCollectionsAsync() {
   yield console.log('fetch collections async fired');
+
+  try {
+    const collectionsRef = firestore.collection('collections');
+    const snapShot = yield collectionsRef.get();
+    const collectionsMap = yield call(
+      convertCollectionsSnapshotToMap,
+      snapShot
+    );
+    yield put(fetchCollectionsSuccess(collectionsMap));
+  } catch (error) {
+    yield put(fetchCollectionsFailure(error.message));
+  }
 }
 
 export function* fetchCollectionsStart() {
