ae555517ba7c9c080b339748b57f01e534d2e67f
Kirsty Mullen <kirstyvox@gmail.com>
2021-01-23 15:41:33 +0000

    refactor: all shop sagas exported

--- src/redux/root-saga.js
+++ src/redux/root-saga.js
@@ -1,8 +1,8 @@
 import {all, call} from 'redux-saga/effects';
 import {userSagas} from './user/user.sagas';
 import {basketSagas} from './basket/basket.sagas';
-import {fetchCollectionsStart} from './shop/shop.sagas';
+import {shopSagas} from './shop/shop.sagas';
 
 export default function* rootSaga() {
-  yield all([call(fetchCollectionsStart), call(userSagas), call(basketSagas)]);
+  yield all([call(shopSagas), call(userSagas), call(basketSagas)]);
 }
