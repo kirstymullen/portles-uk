2e37b5e2f0491508b8c6c2aadfac10060314faaf
Kirsty Mullen <kirstyvox@gmail.com>
2021-01-26 19:58:10 +0000

    refactor: move front end into client folder and rework stripe express server

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PropType from 'prop-types';
 import {loadStripe} from '@stripe/stripe-js';
 import CustomButton from '../custom-buttom/custom-button.component';
+import axios from 'axios';
 
 const StripeButton = ({price}) => {
   const priceForStripe = price * 100;
@@ -11,25 +12,17 @@ const StripeButton = ({price}) => {
   const checkoutClicked = async () => {
     const stripe = await loadStripe(publishableKey);
 
-    const response = await fetch(
-      'http://localhost:4200/create-checkout-session',
-      //'https://portles-uk-api.herokuapp.com/create-checkout-session',
-      {
-        method: 'POST',
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          price: priceForStripe,
-          successUrl: window.location.href,
-          cancelUrl: window.location.href,
-        }),
-      }
-    );
-
-    const sessionData = await response.json();
-    stripe.redirectToCheckout({sessionId: sessionData.id});
+    await axios({
+      url: '/create-stripe-checkout-session',
+      method: 'post',
+      data: {
+        price: priceForStripe,
+        successUrl: window.location.href,
+        cancelUrl: window.location.href,
+      },
+    }).then(response => {
+      stripe.redirectToCheckout({sessionId: response.data.id});
+    });
   };
 
   return <CustomButton onClick={checkoutClicked}>Checkout</CustomButton>;
---
+++ server.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cors());
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+
+  app.get('*', function (req, res) {
+    res.sendFile(pth.join(__dirname, 'client/build', 'index.html'));
+  });
+}
+
+app.listen(port, error => {
+  if (error) {
+    throw error;
+  }
+  console.log('Server running on port ' + port);
+});
+
+app.post('/create-stripe-checkout-session', async (req, res) => {
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ['card'],
+    line_items: [
+      {
+        price_data: {
+          currency: 'gbp',
+          product_data: {
+            name: 'Clothing',
+          },
+          unit_amount: parseInt(req.body.price),
+        },
+        quantity: 1,
+      },
+    ],
+    mode: 'payment',
+    success_url: req.body.successUrl,
+    cancel_url: req.body.cancelUrl,
+  });
+
+  res.json({id: session.id});
+});
