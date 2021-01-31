4f0b46e058637be7810557b056d5058d948b6462
Kirsty Mullen <kirstyvox@gmail.com>
2021-01-31 11:49:59 +0000

    feat: can navigate to specific collection page

--- client/src/components/collection-preview/collection-preview.component.jsx
+++ client/src/components/collection-preview/collection-preview.component.jsx
@@ -1,20 +1,31 @@
 import React from 'react';
+import {useHistory, useRouteMatch} from 'react-router-dom';
 import PropTypes from 'prop-types';
 import './collection-preview.styles.scss';
 import CollectionItem from '../collection-item/collection-item.component';
 
-const CollectionPreview = ({title, items}) => (
-  <div className='collection-preview'>
-    <h1 className='title'>{title.toUpperCase()}</h1>
-    <div className='preview'>
-      {items
-        .filter((item, index) => index < 4)
-        .map(item => (
-          <CollectionItem key={item.id} item={item} />
-        ))}
+const CollectionPreview = ({title, items}) => {
+  const history = useHistory();
+  const match = useRouteMatch();
+
+  return (
+    <div className='collection-preview'>
+      <h1
+        className='title'
+        onClick={() => history.push(`${match.path}/${title.toLowerCase()}`)}
+      >
+        {title.toUpperCase()}
+      </h1>
+      <div className='preview'>
+        {items
+          .filter((item, index) => index < 4)
+          .map(item => (
+            <CollectionItem key={item.id} item={item} />
+          ))}
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 CollectionPreview.propTypes = {
   title: PropTypes.string,
